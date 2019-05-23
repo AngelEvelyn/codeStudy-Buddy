c94d9a10cb6d75af15accbbd0779ce1c568db1e1
Jayvee Aspa <jayveeaspa@Jayvees-MacBook-Pro.local>
2019-05-23 00:31:30 -0700

    finished functionality

--- popup.js
+++ popup.js
@@ -1,3 +1,56 @@
 //use select document by id (.value)
 //put together url
 //fetch request
+
+// const form = document.getElementById('form');
+// const keyword = document.getElementById('keyword').value;
+// console.log(keyword);
+// let newURL = "https://www.google.com/search?q=" + keyword;
+
+
+let language;
+let engine;
+
+function saveSettings(lang, eng) {
+	chrome.storage.sync.set({ 'language': lang, 'eng': eng })
+}
+
+function getSettings() {
+	chrome.storage.sync.get(['language'], function (result) {
+		if (!result) return
+		document.getElementById('language').value = result['language']
+	})
+	chrome.storage.sync.get(['eng'], function (result) {
+		if (!result) return
+		document.getElementById('engine').value = result['eng']
+	})
+}
+
+getSettings();
+
+form.addEventListener('submit', function (event) {
+
+
+
+	const form = document.getElementById('form');
+	const keyword = document.getElementById('keyword').value;
+	language = document.getElementById('language').value
+	engine = document.getElementById('engine').value
+	const googleURL = 'https://www.google.com/search?q='
+	const mdnURL = 'https://developer.mozilla.org/en-US/search?q='
+	let engineChoice;
+
+
+	if (engine === 'google') {
+		engineChoice = googleURL;
+	}
+	else if (engine === 'mdn') {
+		engineChoice = mdnURL;
+	}
+
+	let newURL = engineChoice + keyword + "+" + language;
+
+	saveSettings(language, engine)
+	event.preventDefault();
+	chrome.tabs.create({ url: newURL });
+});
