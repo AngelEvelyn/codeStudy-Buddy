5fa5714e9ce30f8bf0c03bf6f1e9e8bd568064e1
Angel Evelyn <angelvega85@gmail.com>
2019-05-22 20:38:14 -0700

    added files to the folder

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 
+$.ajax({
+  method: 'GET',
+  url: 'http://demo.com',
+  success: (result) => {
+   // result is whatever the URL sends back from the request
+     const data = result
+  },
+  error: function(err) {
+   // if any errors occur during the process you can check out the
+   // the error by logging the 'err' argument
+  }
+};
